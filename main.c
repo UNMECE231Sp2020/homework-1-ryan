c221faf0445c25f287d3094c1689c87e3a6b9950
AnhTran07101998 <60461156+AnhTran07101998@users.noreply.github.com>
2020-02-07 14:26:24 -0700

    Add test

--- main.c
+++ main.c
@@ -4,6 +4,22 @@
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
-
+	
+	Complex (*ptr[4])(Complex, Complex) = {complex_add, complex_sub, complex_mult, complex_div};
+	double (*ptr1[2])(Complex) = {magnitude, phase};
+	
+	int i;
+	
+	for (i=0; i<4; i++)
+	{
+		handler1(ptr[i], a, b);
+	}
+	
+	for (i=0; i<2; i++)
+	{
+		handler2(ptr1[i], a);
+		handler2(ptr1[i], b);	
+	}
+	
 	return 0;
 }
